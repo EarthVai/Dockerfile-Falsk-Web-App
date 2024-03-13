7cb342c16e6917f7f74f03035764c4a75ae47d7b
EarthVai <s6507012660061@email.kmutnb.ac.th>
2024-03-13 12:32:48 +0700

    flask-app version 2

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ def index():
 def sayhi(name):
     return 'Say hi, '+ name
 
+
+@app.route('/sayhi2/<name>', methods=['GET'])
+def sayhi(name):
+    return 'Say hi2, '+ name
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 #port 5000 = flask.
